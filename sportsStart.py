fd146f796b3c5230f25f7082fd07badd9454fba0
plurp411 <66335468+plurp411@users.noreply.github.com>
2021-03-22 22:17:08 -0700

    Add files via upload

--- sportsStart.py
+++ sportsStart.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 # GAMES_FILE_NAME = 'october_games_19-20'
 GAMES_FILE_NAME = 'nov-jan_nba_games'
+TEST_GAMES_FILE_NAME = 'feb_nba_games'
 TEAMS_FILE_NAME = 'nba_teams_19-20'
 
 USE_COLUMNS = ['PTS/GM', 'aPTS/GM', 'PTS DIFF', 'PACE', 'OEFF', 'DEFF', 'EDIFF', 'SOS', 'rSOS', 'SAR', 'CONS', 'A4F', 'W', 'L', 'WIN%', 'eWIN%', 'pWIN%', 'ACH', 'STRK']
@@ -15,9 +16,6 @@ def file_name_to_path(file_name, base_dir="data"):
 
 def get_games_data(file_name):
 
-    # df = pd.read_csv(file_name_to_path(file_name),
-    #     parse_dates=True,
-    #     usecols=['Date', 'Away Team', 'Away Points', 'Home Team', 'Home Points'])
     df = pd.read_csv(file_name_to_path(file_name),
         usecols=['Away Team', 'Away Points', 'Home Team', 'Home Points'])
 
@@ -40,9 +38,6 @@ def get_x_y(games_file_name, teams_file_name):
     # get teams data
     teams_df = get_teams_data(teams_file_name)
 
-    # print(games_df)
-    # print(teams_df)
-
     home_df = pd.DataFrame(columns=teams_df.columns)
     away_df = pd.DataFrame(columns=teams_df.columns)
 
@@ -64,11 +59,6 @@ def get_x_y(games_file_name, teams_file_name):
                 home_df.loc[game_index] = team_row
             if short_team_name in game_row['Away Team']:
                 away_df.loc[game_index] = team_row
-    
-    # print('home_df')
-    # print(home_df)
-    # print('away_df')
-    # print(away_df)
 
     home_df = home_df.drop(columns=['Team'])
     away_df = away_df.drop(columns=['Team'])
@@ -76,17 +66,10 @@ def get_x_y(games_file_name, teams_file_name):
     home_arr = home_df.to_numpy()
     away_arr = away_df.to_numpy()
 
-    # print(home_arr)
-    # print(away_arr)
-
     empty_arr = np.zeros(len(USE_COLUMNS) * 2)
     x = np.array([ empty_arr ])
 
     for i in range(home_arr.shape[0]):
-
-        # data = [home_arr[i], away_arr[i]]
-        # x = np.concatenate((x, [data]), axis=0)
-
         data = np.concatenate((home_arr[i], away_arr[i]), axis=0)
         x = np.concatenate((x, [data]), axis=0)
     
@@ -100,15 +83,15 @@ def test_run():
     x, y = get_x_y(games_file_name=GAMES_FILE_NAME,
         teams_file_name=TEAMS_FILE_NAME)
 
-    print('====================')
-    print()
-    print('x: ', x)
-    print()
-    print('--------------------')
-    print()
-    print('y: ', y)
-    print()
-    print('====================')
+    # print('====================')
+    # print()
+    # print('x: ', x)
+    # print()
+    # print('--------------------')
+    # print()
+    # print('y: ', y)
+    # print()
+    # print('====================')
 
     model = LinearRegression(normalize=True).fit(x, y)
 
@@ -118,27 +101,33 @@ def test_run():
     # print('slope:', model.coef_)
     # print('intercept:', model.intercept_)
 
-    # test_x_arr = np.array([])
-    # test_y_arr = np.array([])
-    # y_pred_arr = np.array([])
-
-    # test_x, test_y = get_x_y(start_date='2000-01-02',
-    #                         end_date=f'2000-03-02', 
-    #                         finle_name=symbol)
+    test_x, test_y = get_x_y(games_file_name=TEST_GAMES_FILE_NAME,
+        teams_file_name=TEAMS_FILE_NAME)
   
-    # y_pred = model.predict(test_x)
+    y_pred = model.predict(test_x)
 
-    # print('SCORE: ', model.score(test_x, test_y))
-   
-    # x_range = range(0, test_y.size)
+    print('prediction score: ', model.score(test_x, test_y))
     
-    # plt.plot(x_range, test_y, 'r')
-    # plt.plot(x_range, y_pred, 'b')
-    # # plt.legend()
-    # # plt.axis([test_x_arr.min() - 1, test_x_arr.max() + 1, test_y_arr.min() - 5, test_y_arr.max() + 5])
-    # plt.show()
+    data_length = test_y.shape[0]
+    x_range = range(data_length)
+
+    test_y = test_y.flatten()
+    test_home = test_y[:data_length]
+    test_away = test_y[data_length:]
+
+    y_pred = y_pred.flatten()
+    home_pred = y_pred[:data_length]
+    away_pred = y_pred[data_length:]
+    
+    plt.plot(x_range, test_home, 'ro', label='Actual Home')
+    plt.plot(x_range, test_away, 'bo', label='Actual Away')
+
+    plt.plot(x_range, home_pred, 'yo', label='Predicted Home')
+    plt.plot(x_range, away_pred, 'go', label='Predicted Away')
+
+    plt.legend(loc='upper left')
+    plt.show()
 
 if __name__ == "__main__":
-    
     test_run()
 
